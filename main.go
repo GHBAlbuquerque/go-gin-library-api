f7593d1bae7bbca6560fad8e804174087c7c339b
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-10-06 15:04:00 -0300

    feat: creates createBook http post method

--- main.go
+++ main.go
@@ -24,10 +24,37 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+/*createBook creates a book and the json version of my book slice*/
+func createBook(c *gin.Context) {
+	var newBook book
+
+	err := c.BindJSON(&newBook)
+
+	if err != nil {
+		return
+	}
+
+	for _, book := range books {
+		if book.ID == newBook.ID {
+			c.JSON(http.StatusConflict, gin.H{"error": "Id already exists"})
+			return
+		}
+
+		if book.Title == newBook.Title && book.Author == newBook.Author {
+			c.JSON(http.StatusConflict, gin.H{"error": "Title from this Author already exists"})
+			return
+		}
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 
 	router.Run("localhost:8080")
 
