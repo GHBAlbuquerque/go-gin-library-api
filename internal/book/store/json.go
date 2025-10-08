00d6e400f6c416b93296b40c609215f4b92c0334
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-10-08 17:03:42 -0300

    feat: creates memory, json and sqlite stores - WIP˜

--- internal/book/store/json.go
+++ internal/book/store/json.go
@@ -13,9 +13,9 @@ type Json struct {
 }
 
 // (m *Json) is my receiver, which can be used to call these functions
-func (m *Json) List(ctx context.Context) ([]book.Book, error)           {}
-func (m *Json) Get(ctx context.Context, id string) (book.Book, error)   {}
-func (m *Json) Create(ctx context.Context, b book.Book) (string, error) {}
-func (m *Json) Update(ctx context.Context, b book.Book) error           {}
-func (m *Json) FindByTitle(ctx context.Context) ([]book.Book, error)    {}
-func (m *Json) FindByAuthor(ctx context.Context) ([]book.Book, error)   {}
+func (m *Json) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
+func (m *Json) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
+func (m *Json) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
+func (m *Json) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
+func (m *Json) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
+func (m *Json) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
--- internal/book/store/memory.go
+++ internal/book/store/memory.go
@@ -12,9 +12,9 @@ type Memory struct {
 }
 
 // (m *Memory) is my receiver, which can be used to call these functions
-func (m *Memory) List(ctx context.Context) ([]book.Book, error)           {}
-func (m *Memory) Get(ctx context.Context, id string) (book.Book, error)   {}
-func (m *Memory) Create(ctx context.Context, b book.Book) (string, error) {}
-func (m *Memory) Update(ctx context.Context, b book.Book) error           {}
-func (m *Memory) FindByTitle(ctx context.Context) ([]book.Book, error)    {}
-func (m *Memory) FindByAuthor(ctx context.Context) ([]book.Book, error)   {}
+func (m *Memory) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
+func (m *Memory) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
+func (m *Memory) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
+func (m *Memory) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
+func (m *Memory) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
+func (m *Memory) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
