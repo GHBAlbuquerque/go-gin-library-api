8f0824e35edd6c881f0f8ad878bd972dbc87fa71
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-10-25 13:28:34 -0300

    feat: JSON store NewJson method impl - WIP

--- internal/book/store/json.go
+++ internal/book/store/json.go
@@ -5,32 +5,58 @@ import (
 	"encoding/json"
 	"errors"
 	"example/go-gin-library-api/internal/book"
+	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 )
 
 type JSON struct {
-	mu   sync.Mutex // Allows one goroutine at a time to access a critical section.
+	mu   sync.Mutex // Allows one go routine at a time to access a critical section.
 	path string
 	data map[string]book.Book
 }
 
-// (m *JSON) is my receiver, which can be used to call these functions
-
-func NewJson(path string, seed []book.Book) (*JSON, error) {
+// (j *JSON) is my receiver, which can be used to call these functions
+// NewJSON creates a JSONstore
+func NewJSON(path string, seed []book.Book) (*JSON, error) {
 	j := &JSON{
 		path: path,
 		data: map[string]book.Book{},
 	}
 
-	// ensure dir on desired path exists
-	if dir := filepath.Dir(path); dir != "." {
-		if err := os.MkdirAll(dir, 0o755); err != nil {
-			return nil, err
+	if err := ensureDir(path); err != nil {
+		return nil, fmt.Errorf("ensureDir: %w", err)
+	}
+
+	created, err := ensureFileWithSeed(path, seed, j)
+	if err != nil {
+		return nil, fmt.Errorf("ensureFileWithSeed: %w", err)
+	}
+
+	if !created {
+		if err := loadFromFile(path, j); err != nil {
+			return nil, fmt.Errorf("loadFromFile: %w", err)
 		}
 	}
 
+	return j, nil
+}
+
+// ensureDir ensures the directory exists on the desired path
+func ensureDir(path string) error {
+	// ensure dir on desired path exists
+	if dir := filepath.Dir(path); dir != "." {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ensureFileWithSeed creates the file with the seed if necessary, and returns a bool (created) to confirm creation
+func ensureFileWithSeed(path string, seed []book.Book, j *JSON) (bool, error) {
 	// load the file from dir
 	_, err := os.Stat(path)
 
@@ -41,30 +67,55 @@ func NewJson(path string, seed []book.Book) (*JSON, error) {
 		}
 
 		if err := j.persist(); err != nil {
-			return nil, err
+			return false, err
 		}
 
-		return j, nil
+		return true, nil
 	}
 
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// loadFromFile reads the Json file from path and unmarshalls the content to the data map on JSON struct
+func loadFromFile(path string, j *JSON) error {
 	bytes, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	if len(bytes) > 0 {
 		err := json.Unmarshal(bytes, &j.data)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	return j, nil
+	return nil
 }
 
-func (m *JSON) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
-func (m *JSON) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
-func (m *JSON) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
-func (m *JSON) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
-func (m *JSON) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
-func (m *JSON) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
+// persist writes the file atomically if it doesn't exist on the path
+func (j *JSON) persist() error {
+	tmp := j.path + ".tmp"
+
+	bytes, err := json.MarshalIndent(j.data, "", "")
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(tmp, bytes, 0o644); err != nil {
+		return err
+	}
+
+	return os.Rename(tmp, j.path)
+}
+
+func (j *JSON) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
+func (j *JSON) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
+func (j *JSON) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
+func (j *JSON) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
+func (j *JSON) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
+func (j *JSON) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
