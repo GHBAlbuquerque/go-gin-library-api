5db8e0b47d808ca3ddb3845b10adc7f7fb1a7b02
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-10-25 12:50:18 -0300

    feat: JSON store impl - WIP

--- internal/book/store/json.go
+++ internal/book/store/json.go
@@ -2,20 +2,69 @@ package stores
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"example/go-gin-library-api/internal/book"
+	"os"
+	"path/filepath"
 	"sync"
 )
 
-type Json struct {
-	mu   sync.Mutex
+type JSON struct {
+	mu   sync.Mutex // Allows one goroutine at a time to access a critical section.
 	path string
 	data map[string]book.Book
 }
 
-// (m *Json) is my receiver, which can be used to call these functions
-func (m *Json) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
-func (m *Json) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
-func (m *Json) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
-func (m *Json) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
-func (m *Json) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
-func (m *Json) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
+// (m *JSON) is my receiver, which can be used to call these functions
+
+func NewJson(path string, seed []book.Book) (*JSON, error) {
+	j := &JSON{
+		path: path,
+		data: map[string]book.Book{},
+	}
+
+	// ensure dir on desired path exists
+	if dir := filepath.Dir(path); dir != "." {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			return nil, err
+		}
+	}
+
+	// load the file from dir
+	_, err := os.Stat(path)
+
+	// use seed to create the file if it doesn't exist
+	if errors.Is(err, os.ErrNotExist) {
+		for _, b := range seed {
+			j.data[b.ID] = b
+		}
+
+		if err := j.persist(); err != nil {
+			return nil, err
+		}
+
+		return j, nil
+	}
+
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(bytes) > 0 {
+		err := json.Unmarshal(bytes, &j.data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return j, nil
+}
+
+func (m *JSON) List(ctx context.Context) ([]book.Book, error)           { /*TODO*/ }
+func (m *JSON) Get(ctx context.Context, id string) (book.Book, error)   { /*TODO*/ }
+func (m *JSON) Create(ctx context.Context, b book.Book) (string, error) { /*TODO*/ }
+func (m *JSON) Update(ctx context.Context, b book.Book) error           { /*TODO*/ }
+func (m *JSON) FindByTitle(ctx context.Context) ([]book.Book, error)    { /*TODO*/ }
+func (m *JSON) FindByAuthor(ctx context.Context) ([]book.Book, error)   { /*TODO*/ }
