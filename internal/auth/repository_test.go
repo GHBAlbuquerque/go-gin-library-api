96d2f8e2509ac850ad7db7f103d6f2968daa7a87
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-11-15 18:53:22 -0300

    test: client repository unit tests˜

--- internal/auth/repository_test.go
+++ internal/auth/repository_test.go
@@ -1,3 +1,38 @@
 package auth_test
 
-//TODO
+import (
+	"example/go-gin-library-api/internal/auth"
+	"testing"
+)
+
+// ---------- Tests ----------
+
+func TestRepository_Validate_Success(t *testing.T) {
+	repo := auth.NewInMemoryClientRepo(map[string]string{"id": "secret"})
+
+	result := repo.Validate("id", "secret")
+
+	if !result {
+		t.Fatal("Valid client was deemed invalid.")
+	}
+}
+
+func TestRepository_Validate_ClientDoesNotExistFailure(t *testing.T) {
+	repo := auth.NewInMemoryClientRepo(map[string]string{"id": "secret"})
+
+	result := repo.Validate("not id", "secret")
+
+	if result {
+		t.Fatal("Inexistent client was found.")
+	}
+}
+
+func TestRepository_Validate_BadSecretFailure(t *testing.T) {
+	repo := auth.NewInMemoryClientRepo(map[string]string{"id": "secret"})
+
+	result := repo.Validate("id", "bad secret")
+
+	if result {
+		t.Fatal("Invalid client was deemed valid.")
+	}
+}
